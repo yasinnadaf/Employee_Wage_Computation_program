063449b73f60e6ba7fb689516f62fd7bb4af936f
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-06-22 07:09:47 -0700

    [yasin]code is refactored

---
+++ Q9StoringDayAndDailwagesAlongWithTotalWAges.sh
@@ -0,0 +1,47 @@
+#! /bin/bash -x
+
+declare -A Wage
+
+NUM_OF_WORKING_DAYS=20
+WAGE_PER_HR=20
+FULL_DAY_HRS=8
+PART_TIME_HRS=4
+
+daily_Wage=0
+monthly_Wage=0
+
+for i in `seq $NUM_OF_WORKING_DAYS`
+do
+	random=$(( $RANDOM % 3 ))
+
+
+        case $random in
+
+
+        1)
+                echo "Present - FullDay"
+                daily_Wage=$(( $WAGE_PER_HR * $FULL_DAY_HRS ))
+        ;;
+        2)
+                echo "Present - HalfDay"
+                daily_Wage=$(( $WAGE_PER_HR * $PART_TIME_HRS ))
+        ;;
+        *)
+                echo "Absent"
+		daily_Wage=0
+        ;;
+
+        esac
+
+	Wage[$i]=$daily_Wage
+	
+monthly_Wage=$(( $monthly_Wage + $daily_Wage ))
+
+done
+
+	Wage[$(( $i + 1 ))]=$monthly_Wage
+
+for i in `seq 21`
+do
+        echo "$i - ${Wage[$i]}"
+done
